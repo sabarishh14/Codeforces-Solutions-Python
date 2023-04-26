5278bec7f7188a5b0af614621d07d5d2298795ee
sabarishh14 <110752601+sabarishh14@users.noreply.github.com>
2023-04-26 21:13:01 +0530

    267A - Subtraction (Python)

---
+++ 267A - Subtractions.py
@@ -0,0 +1,12 @@
+n = int(input())
+for i in range(n):
+    a, b = map(int, input().split())
+    total = 0
+    while a > 0 and b > 0:
+        if a > b:
+            total += a // b
+            a -= (a // b) * b
+        else:
+            total += b // a
+            b -= (b // a) * a
+    print(total)
